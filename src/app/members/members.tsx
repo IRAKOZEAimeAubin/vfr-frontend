3fde9d64275bc787f452c63c70886f50fefce969
Irakoze Aime Aubin <irakozeaimeaubin@gmail.com>
2023-09-02 18:07:22 +0200

    feat: add delete and edit buttons

--- src/app/members/members.tsx
+++ src/app/members/members.tsx
@@ -5,6 +5,7 @@ import getMembers from '../services/getMembers'
 import {MemberType} from '../types/Member'
 import {useState} from 'react'
 import CreateMember from './createMember'
+import {AiFillDelete, AiFillEdit} from 'react-icons/ai'
 
 export default function MembersTable({token}: {token: string}) {
   const [createToggle, setCreateToggle] = useState(false)
@@ -120,57 +121,9 @@ export default function MembersTable({token}: {token: string}) {
                       <td className='px-4 py-3'>{member.department}</td>
                       <td className='px-4 py-3'>{member.phone}</td>
                       <td className='px-4 py-3'>{member.monthlyPledge}</td>
-                      <td className='px-4 py-3 flex items-center justify-end'>
-                        <button
-                          id='apple-imac-27-dropdown-button'
-                          data-dropdown-toggle='apple-imac-27-dropdown'
-                          className='inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100'
-                          type='button'
-                        >
-                          <svg
-                            className='w-5 h-5'
-                            aria-hidden='true'
-                            fill='currentColor'
-                            viewBox='0 0 20 20'
-                            xmlns='http://www.w3.org/2000/svg'
-                          >
-                            <path d='M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z' />
-                          </svg>
-                        </button>
-                        <div
-                          id='apple-imac-27-dropdown'
-                          className='hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600'
-                        >
-                          <ul
-                            className='py-1 text-sm text-gray-700 dark:text-gray-200'
-                            aria-labelledby='apple-imac-27-dropdown-button'
-                          >
-                            <li>
-                              <a
-                                href='#'
-                                className='block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
-                              >
-                                Show
-                              </a>
-                            </li>
-                            <li>
-                              <a
-                                href='#'
-                                className='block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'
-                              >
-                                Edit
-                              </a>
-                            </li>
-                          </ul>
-                          <div className='py-1'>
-                            <a
-                              href='#'
-                              className='block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'
-                            >
-                              Delete
-                            </a>
-                          </div>
-                        </div>
+                      <td className='px-4 py-3 flex items-center justify-evenly text-xl'>
+                        <AiFillDelete className='bg-red-500 p-[2px] text-red-200 rounded' />
+                        <AiFillEdit className='bg-green-500 text-green-200 p-[2px] rounded' />
                       </td>
                     </tr>
                   ))}
